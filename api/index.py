1c37e4acaff5360255cd28100b0c33d069718297
txtyb <txtybbill@163.com>
2022-05-16 02:39:44 +0800

    Adapt to frontend's temperature and humidity overrun notification warning [1/2]

--- api/index.py
+++ api/index.py
@@ -42,6 +42,24 @@ def put():
     timestamp = int(time())
     [data['time'], data['tem'], data['rh']] = [timestamp, tem, rh]
     # print(data)
+    # if over the warning limit then post
+    warningValues = r.get('warningValues')
+    if warningValues is not None:
+        warningValues = warningValues.decode('utf-8')
+        warningValues = json.loads(warningValues)
+        [temWarningValue, rhWarningValue] = [warningValues['tem'], warningValues['rh']]
+        temFlag = 0
+        rhFlag = 0
+
+        if temWarningValue:
+            if data['tem'] >= temWarningValue:
+                temFlag = 1
+        if rhWarningValue:
+            if data['rh'] >= rhWarningValue:
+                rhFlag = 1
+        
+        if (temFlag or rhFlag):
+            sendtofcm(innerSend=1, rowDataTem=data['tem'], rowDataRh=data['rh'], datetime=to_datetime(timestamp), temFlag=temFlag, rhFlag=rhFlag, temWarningValue=temWarningValue, rhWarningValue=rhWarningValue)
 
     nowdata = list()
     try:
@@ -63,6 +81,43 @@ def put():
     r.set('data', json.dumps(nowdata))
     
     return jsonify(data)
+
+
+@app.route('/api/setWarningValues', methods=['GET'])
+def setWarningValues():
+    tem = request.args.get('tem', type=float, default=None)
+    rh = request.args.get('rh', type=float, default=None)
+    dictData = {
+        'tem': tem, 
+        'rh': rh
+    }
+    r.set('warningValues', json.dumps(dictData))
+    return jsonify(dictData)
+
+
+@app.route('/api/getWarningValues', methods=['GET'])
+def getWarningValues():
+    warningValues =  r.get('warningValues')
+    dictData =  {
+        'tem': None, 
+        'rh': None
+    }
+    if warningValues is not None:
+        warningValues = warningValues.decode('utf-8')
+        warningValues = json.loads(warningValues)
+
+        if warningValues['tem'] is not None:
+            dictData['tem'] = warningValues['tem']
+        if warningValues['rh'] is not None:
+            dictData['rh'] = warningValues['rh']
+    
+    return jsonify(dictData)
+
+# set warning flags. If the tem or rh is higher than flag, then temWarning or rhWarning == 1
+@app.route('/api/clearWarningValues', methods=['GET'])
+def clearWarningValues():
+    r.delete('warningValues')
+    return 'OK'
 
 
 @app.route('/api/get', methods=['GET'])
@@ -176,15 +231,49 @@ def warningindex():
 
 
 @app.route('/api/sendtofcm', methods=['POST'])
-def sendtofcm():
+def sendtofcm(innerSend=None, rowDataTem=None, rowDataRh=None, datetime=None, temFlag=0, rhFlag=0, temWarningValue=None, rhWarningValue=None):
     url = 'https://fcm.googleapis.com/fcm/send'
-    body = request.json
-    authorization = request.headers.get('Authorization', type=str)
-    headers = {
-        'Authorization': authorization
-    }
-    response = requests.post(url, headers=headers, json=body)
-    return jsonify(response.text), response.status_code
+    authorization = 'key=AAAASwElybY:APA91bFaTT_zKLcLYqB0soW8PJmFFG7x1F3wiR0MGta9lLsU22uAVa0VD_3zzz-OremJKDEWEf52OD554byamcwAmZldgrQKfwAjjbhZz_5DYT-z1gcflUBFSWVQQ9lSE9KwDBNHULvfVKmQwxa7xNwuPHz-VfdTbw'
+    token = None
+    tmp = r.get('fcmToken')
+    if tmp:
+        token = tmp.decode('utf-8') 
+    if innerSend:
+        warningMsg = '%s%s'%('温度过高！' if temFlag else '', ' 湿度过高！' if rhFlag else '')
+        warningValueMsg = '您的%s%s'%('温度限制为%.1f℃'%(temWarningValue) if temWarningValue else '', ' 湿度限制为%.1fRh%%'%(rhWarningValue) if rhWarningValue else '')
+        body = '{ "to": "%s", "time_to_live": 60, "priority": "high", "data": { "text": { "title": "%s", "message": "%s 温度: %.1f℃, 湿度: %.1fRh%% %s", "clipboard": false } } }'%(token, datetime, warningMsg, rowDataTem, rowDataRh, warningValueMsg)
+        headers = {
+            # 'Content-Type': 'application/json', 
+            'Authorization': authorization 
+        }
+        response = requests.post(url, headers=headers, json=json.loads(body))
+        # print(response.text)
+        return
+    else:
+        body = request.json
+        authorization = request.headers.get('Authorization', type=str)
+        headers = {
+            'Authorization': authorization
+        }
+        response = requests.post(url, headers=headers, json=body)
+        return jsonify(response.text), response.status_code
+
+
+@app.route('/api/setFcmToken', methods=['GET'])
+def setFcmToken():
+    token = request.args.get('token', type=str, default=None)
+    if token:
+        r.set('fcmToken', token)
+    return jsonify({
+        'msg': 'success', 
+        'token': token
+    })
+
+
+@app.route('/api/clearFcmToken', methods=['GET'])
+def clearFcmToken():
+    r.delete('fcmToken')
+    return 'OK'
 
 
 if __name__ == "__main__": 
